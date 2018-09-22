1ac154fc50f21a76c85990a10cc4fde599d707a0
QwertygidQ <Qwertygid@gmail.com>
2018-09-22 21:51:10 +0300

    Something else

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from . import app
 from .forms import CSV_Form
 from flask import render_template, request, flash, redirect, url_for
 import pandas
+import random
 
 def get_answer(params):
     manipulables = ['sect1_flow_16', 'sect1_flow_22', 'sect1_temperature_1',
@@ -18,6 +19,8 @@ def index():
         if file:
             file.save('upload.csv')
             df = pandas.read_csv('upload.csv')
+            print(df)
+            print(dir(df))
             params = dict(df.iloc[0])
 
             return render_template('index.html', form=csv_form, optimized=get_answer(params))
