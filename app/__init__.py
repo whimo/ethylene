8a36d5707433df8b0bb095d08ebfb80a2bf03ac1
2toxic <isaqtm@gmail.com>
2018-09-23 13:26:17 +0300

    Presentation, guys

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+import pandas
+from .model import Model
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+attrs = pandas.read_excel('mk_dict.xlsx')
+data = pandas.read_csv('mk_data.csv')
+predictor = Model()
+
+data_train = data
+predictor.fit(data_train)
+
+constrained = list(attrs[attrs['Constraint'] < 2]['Variable'])
+manipulated = list(attrs[attrs['Manipulated'] < 2]['Variable'])
+non_constrained = [m for m in manipulated if m not in constrained]
+non_manipulated = [c for c in constrained if c not in manipulated]
 
 from . import views
